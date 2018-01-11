ede03f193b0ca355f27cad344bc72e1fd9b36601
Ben WU <wubin28@gmail.com>
2018-01-11 09:41:22 +0800

    rename

--- src/main/java/org/katas/refactoring/OrderReceipt.java
+++ src/main/java/org/katas/refactoring/OrderReceipt.java
@@ -17,7 +17,6 @@ public class OrderReceipt {
 	public String printReceipt() {
 		StringBuilder output = new StringBuilder();
 
-		// print headers
 		output.append("======Printing Orders======\n");
 
 		// print date, bill no, customer name
@@ -27,8 +26,8 @@ public class OrderReceipt {
 //        output.append(order.getCustomerLoyaltyNumber());
 
 		// prints lineItems
-		double totSalesTx = 0d;
-		double tot = 0d;
+		double totalSalesTax = 0d;
+		double totalAmountOfLineItems = 0d;
 		for (LineItem lineItem : order.getLineItems()) {
 			output.append(lineItem.getDescription());
 			output.append('\t');
@@ -41,17 +40,17 @@ public class OrderReceipt {
 
 			// calculate sales tax @ rate of 10%
             double salesTax = lineItem.totalAmount() * .10;
-            totSalesTx += salesTax;
+            totalSalesTax += salesTax;
 
             // calculate total amount of lineItem = price * quantity + 10 % sales tax
-            tot += lineItem.totalAmount() + salesTax;
+            totalAmountOfLineItems += lineItem.totalAmount() + salesTax;
 		}
 
 		// prints the state tax
-		output.append("Sales Tax").append('\t').append(totSalesTx);
+		output.append("Sales Tax").append('\t').append(totalSalesTax);
 
         // print total amount
-		output.append("Total Amount").append('\t').append(tot);
+		output.append("Total Amount").append('\t').append(totalAmountOfLineItems);
 		return output.toString();
 	}
 }
