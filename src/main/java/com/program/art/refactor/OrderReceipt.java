507935e3970e29927a1b63db1495445f8180ea6a
Bin WU <wubinben@icloud.com>
2020-10-27 21:13:04 +0800

    move total sales tax of all line items calculation logic to Order - extract method

--- src/main/java/com/program/art/refactor/OrderReceipt.java
+++ src/main/java/com/program/art/refactor/OrderReceipt.java
@@ -33,16 +33,21 @@ public class OrderReceipt {
 			totalAmount += lineItem.totalAmount() + salesTax;
 		}
 
-		double totalSalesTax = 0d;
-		for (LineItem lineItem : order.getLineItems()) {
-			double salesTax = lineItem.salesTax();
-			totalSalesTax += salesTax;
-		}
+		double totalSalesTax = totalSalesTax();
 
 		output.append('\n').append("Sales Tax").append('\t').append(totalSalesTax).append('\n');
 
 		output.append("Total Amount").append('\t').append(totalAmount);
 		return output.toString();
+	}
+
+	private double totalSalesTax() {
+		double totalSalesTax = 0d;
+		for (LineItem lineItem : order.getLineItems()) {
+			double salesTax = lineItem.salesTax();
+			totalSalesTax += salesTax;
+		}
+		return totalSalesTax;
 	}
 
 }
